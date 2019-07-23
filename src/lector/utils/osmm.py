96cd7a59e425e93d60d1fcc96592d7984d2baf74
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-07-24 00:56:19 +0200

    Impelement auto graphhopper restart on data change, Implement clean data on graphhopper restart, Implement automatic map download, Impelemnt automatic map save with graphhopper restart

--- src/lector/utils/osmm.py
+++ src/lector/utils/osmm.py
@@ -1,16 +1,30 @@
 import osmnx as ox
 import docker
 import re
+import os
+import shutil
 
-BAMBERG_BBOX = [49.865874134216426, 49.925145775384436, 10.836982727050781, 10.951995849609375]
+# BAMBERG_BBOX = [49.865874134216426, 49.925145775384436, 10.836982727050781, 10.951995849609375]
+BAMBERG_BBOX = [49.9954, 49.7511, 10.7515, 11.1909]
 SERVICE_NAME = 'graphhopper'
+OSM_OUTPUT_FILENAME = "data"
+OSM_OUTPUT_DIR = "/osm_data"
 
 
 def download_map():
     G = ox.graph_from_bbox(49.925145775384436, 49.865874134216426, 10.951995849609375, 10.836982727050781,
                            network_type='all')
-    ox.save_graph_osm(G, filename='bamberg.osm', folder='/osm_data')
+    # G = ox.graph_from_address('350 5th Ave, New York, New York', network_type='all')
+    # G = ox.graph_from_address('Berlin, 10117, Deutschland', network_type='all')
+    # G = ox.graph_from_address('Bamberg, Oberfranken, Bayern, 96047, Deutschland', network_type='all', distance=5000)
+    # G = ox.graph_from_address('Erlangen, Mittelfranken, Bayern, 91052, Deutschland', network_type='all')
+    ox.save_graph_osm(G, filename=f'{OSM_OUTPUT_FILENAME}.osm', folder=OSM_OUTPUT_DIR)
+    remove_current_graphhopper_data()
     restart_container_by_service_name(SERVICE_NAME)
+
+
+def load_map():
+    return ox.graph_from_file(f'{OSM_OUTPUT_DIR}/{OSM_OUTPUT_FILENAME}.osm')
 
 
 def get_container_by_service_name(name):
@@ -24,4 +38,21 @@ def get_container_by_service_name(name):
 
 def restart_container_by_service_name(container_name):
     container = get_container_by_service_name(container_name)
-    container.restart()
+    # TODO logging if container is not running
+    if container:
+        container.restart()
+
+
+def remove_current_graphhopper_data():
+    shutil.rmtree(f'{OSM_OUTPUT_DIR}/{OSM_OUTPUT_FILENAME}-gh', ignore_errors=True)
+
+
+def plot_graph(G):
+    ox.plot_graph(G, save=True, file_format='svg', filename=f'{OSM_OUTPUT_DIR}/network_plot')
+
+
+if __name__ == '__main__':
+    G = ox.graph_from_address('Bamberg, Oberfranken, Bayern, 96047, Deutschland', network_type='all', distance=500)
+    print("GRAPH LOADED")
+    plot_graph(G)
+    print("GRAPH PLOTTED")
